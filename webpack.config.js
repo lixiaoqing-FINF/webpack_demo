d329c7bfdf02b18dea1941e1078943724d813faa
李清清 <1335455480@qq.com>
2018-06-17 10:46:31 +0800

    多入口

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path'); //引入node的path模块
 module.exports = {
     entry: {
-        entry: './src/entry.js'
+        entry: './src/entry.js',
+        entry2: './src/entry2.js'
     }, //入口
     output: {
         path: path.resolve(__dirname, 'dist'), //生成dist的绝对路径
-        filename: 'bundle.js'
+        filename: '[name].js'
     }, //出口
     module: {}, //模块
     plugins: [], //插件
