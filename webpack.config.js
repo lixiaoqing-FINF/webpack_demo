ee813bc22c6dc620312f9ab1f6311a79b69c89b3
李清清 <1335455480@qq.com>
2018-09-13 19:00:38 +0800

    打包css文件

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,14 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'), //生成dist的绝对路径
         filename: '[name].js'
     }, //出口
-    module: {}, //模块
+    module: {
+        rules: [
+           {
+               test: /\.css$/,
+               use: ['style-loader', 'css-loader']
+           } 
+        ]
+    }, //模块
     plugins: [], //插件
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'), //监听dist文件
