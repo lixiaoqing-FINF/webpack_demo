6d0299ce819174feff6a6f1b4f63afc9281ce574
李清清 <1335455480@qq.com>
2018-09-13 18:08:51 +0800

    配置文件：服务和热更新

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,10 @@ module.exports = {
     }, //出口
     module: {}, //模块
     plugins: [], //插件
-    devServer: {} //服务
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //监听dist文件
+        host: 'localhost', //建议写本机的ip地址
+        compress: true, //服务端是否压缩
+        port: 1224 //配置端口号（随便配）
+    } //服务
 }
